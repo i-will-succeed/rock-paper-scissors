6fde5bfe67a310428e7afcbd37870f53d3e6fe28
i-will-succeed <sagonzalez0305@gmail.com>
2022-06-23 14:12:45 -0400

    Fix Event listener, clean-up logic, more CSS styling, Stop Event Listener after 5 points is reached.

--- script.js
+++ script.js
@@ -1,42 +1,15 @@
 // Main Variables
 const buttons = document.querySelectorAll(".button");
-const gameResults = document.querySelector('.gameResults');
-
-const playerScoreDisplayed = document.getElementById('playerScore')
-const computerScoreDisplayed = document.getElementById('computerScore');
-
-
-
+const playerScoreDisplayed = document.getElementById("playerScore");
+const computerScoreDisplayed = document.getElementById("computerScore");
+const gameResults = document.querySelector(".gameResults");
+const endResults = document.querySelector(".endResults")
 let computerScore = 0;
 let playerScore = 0;
+let playerSelection;
 
-// TODO --> Figure out how to remove event listener, clean up code, instead of using button.id,'
-// maybe create variable (playerSelection) from the information in event listener,
-// Figure out why Score Displays are acting funny
-
-// Listens for button click, and applies button.id value to playerSelection
-buttons.forEach((button) => {
-  button.addEventListener("click", () => {
-    playerSelection = button.id
-    const computerSelection = computerPlay().toLowerCase();
-
-    
-    gameResults.textContent = (playRound(playerSelection, computerSelection));
-
-    // playerScoreDisplayed.style.cssText = 'background-color: blueviolet; box-shadow: rgba(138, 43, 226, 0.16) 0px 3px 6px, rgba(138, 43, 226, 0.23) 0px 3px 6px;';
-    // computerScoreDisplayed.style.cssText = 'background-color: blueviolet; box-shadow: rgba(138, 43, 226, 0.16) 0px 3px 6px, rgba(138, 43, 226, 0.23) 0px 3px 6px;';   
-
-    playerScoreDisplayed.textContent = `Your Score: ${playerScore}`;
-    computerScoreDisplayed.textContent = `Computer Score: ${computerScore}`;
-
-    if (playerScore === 5 || computerScore === 5) {
-      gameResults.textContent = `Final SCORE:\nYour score is: ${playerScore}, Computer Score is: ${computerScore}`
-      button.removeEventListener('click', this);
-    }
-
-  })
-});
-
+// Hides End Results
+endResults.style.visibility= "hidden";
 
 // Generates random number between 0 and 2,
 // and applies 'rock', 'paper', or 'scissors' to value
@@ -54,35 +27,68 @@ function computerPlay() {
 // LOGIC: Compares, keeps score, and returns result
 function playRound(playerSelection, computerSelection) {
   if (playerSelection === "rock" && computerSelection === "rock") {
-    return "It's a Tie! You both chose Rock!";
+    return "It's a Tie!\nYou both chose Rock!";
   } else if (playerSelection === "rock" && computerSelection === "paper") {
     computerScore += 1;
-    return "You Lose! Paper Beats Rock!";
+    return "You Lose!\nPaper Beats Rock!";
   } else if (playerSelection === "rock" && computerSelection === "scissors") {
     playerScore += 1;
-    return "You Win! Rock Beats Scissors!";
+    return "You Win!\nRock Beats Scissors!";
   } else if (playerSelection === "paper" && computerSelection === "paper") {
-    return "It's a Tie! You both chose Paper!";
+    return "It's a Tie!\nYou both chose Paper!";
   } else if (playerSelection === "paper" && computerSelection === "scissors") {
     computerScore += 1;
-    return "You Lose! Scissors Beats Paper!";
+    return "You Lose!\nScissors Beats Paper!";
   } else if (playerSelection === "paper" && computerSelection === "rock") {
     playerScore += 1;
-    return "You Win! Paper Beats Rock!";
+    return "You Win!\nPaper Beats Rock!";
   } else if (
     playerSelection === "scissors" &&
     computerSelection === "scissors"
   ) {
-    return "It's a Tie! You both chose Scissors!";
+    return "It's a Tie!\nYou both chose Scissors!";
   } else if (playerSelection === "scissors" && computerSelection === "rock") {
     computerScore += 1;
-    return "You Lose! Rock Beats Scissors!";
+    return "You Lose!\nRock Beats Scissors!";
   } else if (playerSelection === "scissors" && computerSelection === "paper") {
     playerScore += 1;
-    return "You Win! Scissors Beats Paper!";
+    return "You Win!\nScissors Beats Paper!";
   } else {
     return "Invalid Input";
   }
+}
+
+function getPlayerSelection(e) {
+  playerSelection = e.target.alt;
+  gameResults.innerText = playRound(playerSelection, computerPlay());
+  compareScores()
+}
+
+function compareScores() {
+  playerScoreDisplayed.textContent = `Your Score: ${playerScore}`;
+  computerScoreDisplayed.textContent = `Computer Score: ${computerScore}`;
+
+  if (playerScore === 5 || computerScore === 5) {
+    buttons.forEach((button) => {
+      button.removeEventListener("click", getPlayerSelection);
+    });
+    gameResults.innerText = `Final Score\n\nYour score is: ${playerScore}\nComputer Score is: ${computerScore}`
+    endResults.style.visibility= "visible";
+    if (playerScore > computerScore) {
+      endResults.textContent = 'VICTORY!'
+    } else {
+      endResults.textContent = 'DEFEAT!'
+    }
+}
+}
+
+// Listens for button click, and applies button.id value to playerSelection
+buttons.forEach((button) => {
+  button.addEventListener("click", getPlayerSelection);
+});
+
+function playAgain() {
+
 }
 
 
